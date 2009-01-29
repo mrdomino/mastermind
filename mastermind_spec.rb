a4154643e6623275cddbf1e4975856371aa8e101
Steven Dee <steve@smartercode.net>
2009-01-29 15:49:14 -0500

    initial spec for mastermind describes what we want

---
+++ mastermind_spec.rb
@@ -0,0 +1,6 @@
+describe Mastermind do
+  it "should pass when given a correct solution"
+  it "should fail when given an incorrect solution"
+  it "should indicate when a correct peg is placed"
+  it "should indicate when a correct color is placed"
+end
