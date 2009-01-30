f4aa6909e7746b874acc8ecad65df5dfc0f325b4
Steven Dee <steve@smartercode.net>
2009-01-30 16:45:21 -0500

    new spec based on actual mastermind rules

--- mastermind.rb
+++ mastermind.rb
@@ -1,19 +1,2 @@
 class Mastermind
-  def initialize(game)
-    @game = game
-  end
-  def game_size
-    @game.size
-  end
-  def try(game)
-    if game == @game then
-      @solved = true
-    end
-  end
-  def solved?
-    @solved
-  end
-  def correct_pegs
-    1
-  end
 end
--- mastermind_spec.rb
+++ mastermind_spec.rb
@@ -1,28 +1,33 @@
 require 'mastermind'
 
 describe Mastermind do
-  before(:each) do
-    @mastermind = Mastermind.new [1, 2, 3, 4]
+
+  describe "color selection" do
+    it "should allow specification of the number of colors"
+    it "should default to 6 colors"
+    it "should accept valid color choices"
+    it "should reject invalid color choices"
   end
-  it "should pass when given a correct solution" do
-    @mastermind.try [1,2,3,4]
-    @mastermind.should be_solved
+
+  describe "guessing" do
+    it "should allow specification of the number of pegs in a row"
+    it "should default to 4 pegs per row"
+    it "should allow specification of the number of guesses"
+    it "should default to 10 guesses"
+    it "should accept valid solutions"
+    it "should not accept invalid solutions"
+
+    describe "hints", :shared => true do
+      it "should show 0 pegs if none are right"
+      it "should show n pegs if n are right"
+    end
+
+    describe "(with correctly-colored pegs in the right place)" do
+      it_should_behave_like "hints"
+    end
+
+    describe "(with correctly-colored pegs in the wrong place)" do
+      it_should_behave_like "hints"
+    end
   end
-  it "should fail when given an incorrect solution" do
-    @mastermind.try [1,2,3,5]
-    @mastermind.should_not be_solved
-  end
-  it "should indicate when a correct peg is placed" do
-    @mastermind.try [1,5,6,7]
-    @mastermind.correct_pegs.should == 1
-  end
-  it "should indicate how many correct pegs were placed" do
-    @mastermind.try [1,2,3,5]
-    @mastermind.correct_pegs.should == 3
-  end
-  it "should have all correct pegs for a solved game" do
-    @mastermind.try [1,2,3,4]
-    @mastermind.correct_pegs.should == @mastermind.game_size
-  end
-  it "should indicate when a correct color is placed"
 end
