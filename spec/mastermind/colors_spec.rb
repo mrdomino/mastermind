75a0d93964c0a459cd783620355d78933a325cdc
Steven Dee <steve@smartercode.net>
2009-02-05 04:25:58 -0500

    quick and dirty colors spec

---
+++ spec/mastermind/colors_spec.rb
@@ -0,0 +1,26 @@
+require File.join(File.dirname(__FILE__), %w(.. spec_helper))
+
+require 'mastermind/colors'
+
+describe Mastermind::Colors do
+  before do
+    @colorss = [6,8,10].collect {|n| Mastermind::Colors.colors n }
+  end
+  it "should return a unique list of colors" do
+    @colorss.each do |cs|
+      cs.dup.each_with_index do |x,i|
+        cs.each_with_index do |y,j|
+          x.should_not == y unless i == j
+        end
+      end
+    end
+  end
+  it "should return colors in RGB format" do
+    @colorss.each do |cs|
+      cs.each do |c|
+        c.size.should == 3
+        c.each {|x| x.should be_between 0,1 }
+      end
+    end
+  end
+end
